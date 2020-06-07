3be2b297382453b1604435c08f5cda99f6364191
Vijay Mukhija <mukhijavijay@gmail.com>
2020-06-07 18:39:33 +0530

    Added Seach Part

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,39 @@
 // Global app controller
+import Search from './models/Search';
+import { elements, renderLoader, clearLoader } from './views/base';
+import * as searchView from './views/searchView';
+
+/*  Global State of the App
+** - Search Object
+** - Current Recipe Object
+** - Shopping List object
+** - Likes object
+*/
+const state = {}
+
+const searchController = async () => {
+    //get query from view
+    const query = searchView.getInput();
+
+    if (query) {
+        //new search object and add to state
+        state.search = new Search(query);
+
+        //Prepare UI for results
+        searchView.clearInput();
+        searchView.clearResults();
+        renderLoader(elements.searchRes);
+
+        //Search query 
+        await state.search.getResults();
+
+        //Render results on UI
+        clearLoader();
+        searchView.renderResults(state.search.result);
+    }
+
+}
+elements.searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    searchController();
+})
---
+++ src/js/views/base.js
@@ -0,0 +1,28 @@
+//This object will have all the Dom Query Selectors
+export const elements = {
+    searchForm: document.querySelector('.search'),
+    searchInput: document.querySelector('.search__field'),
+    searchResList: document.querySelector('.results__list'),
+    searchRes: document.querySelector('.results'),
+}
+
+export const elementStrings = {
+    loader: 'loader'
+};
+
+//loading spinner for Ajax Request
+export const renderLoader = parent => {
+    const loader = `
+        <div class="${elementStrings.loader}">
+            <svg>
+                <use href="img/icons.svg#icon-cw"></use>
+            </svg>
+        </div>
+    `;
+    parent.insertAdjacentHTML('afterbegin', loader);
+};
+
+export const clearLoader = () => {
+    const loader = document.querySelector(`.${elementStrings.loader}`);
+    if (loader) loader.parentElement.removeChild(loader);
+};
